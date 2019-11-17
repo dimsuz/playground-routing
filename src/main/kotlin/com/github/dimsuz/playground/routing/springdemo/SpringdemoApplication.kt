3bbdb5938c30239c5d3c9a202bc2069ec784192e
Dmitry Suzdalev <dimsuz@gmail.com>
2019-11-17 02:15:20 +0200

    Experiment with plain routing flow (no nesting)

--- src/main/kotlin/com/github/dimsuz/playground/routing/springdemo/SpringdemoApplication.kt
+++ src/main/kotlin/com/github/dimsuz/playground/routing/springdemo/SpringdemoApplication.kt
@@ -9,22 +9,68 @@ import org.springframework.statemachine.StateMachine
 @SpringBootApplication
 class SpringdemoApplication : CommandLineRunner {
   @Autowired
-  private lateinit var stateMachine: StateMachine<States, Events>
+  private lateinit var stateMachine: StateMachine<Route, Event>
 
   override fun run(vararg args: String?) {
-    stateMachine.sendEvent(Events.E1)
-    stateMachine.sendEvent(Events.E2);
-    stateMachine.sendEvent(Events.E2);
+    stateMachine.sendEvent(Event.LoginSuccessOtpRequired)
+    stateMachine.sendEvent(Event.BackPressed)
+    stateMachine.sendEvent(Event.LoginSuccessOtpNotRequired)
+    stateMachine.sendEvent(Event.BackPressed)
+
+    stateMachine.sendEvent(Event.LoginSuccessOtpRequired)
+    stateMachine.sendEvent(Event.OtpIntroContinueRequest)
+    stateMachine.sendEvent(Event.OtpInputSuccessful)
+    stateMachine.sendEvent(Event.PinIntroContinueRequest)
+    stateMachine.sendEvent(Event.PinCreateSuccess)
+    stateMachine.sendEvent(Event.BackPressed)
   }
 }
 
-enum class States {
-  SI, S1, S2
+enum class Flow {
+  Login,
+  Otp,
+  PinCreate,
+  PinValidate,
+  Main
 }
 
-enum class Events {
-  E1, E2
+enum class Route {
+  Login,
+  PinIntro,
+  PinCreate,
+  FingerprintIntro,
+  FingerprintCreate,
+  PinValidate,
+  FingerprintValidate,
+  OtpIntro,
+  OtpInput,
+  Main,
+  BooksMain,
+  MoviesMain,
+  BookDetails,
+  BookPurchase,
+  MovieDetails,
+  MoviePurchase,
+
+  Exit
 }
+
+sealed class Event {
+  object BackPressed : Event()
+  object LoginSuccessOtpRequired : Event()
+  object LoginSuccessOtpNotRequired : Event()
+  object OtpIntroContinueRequest : Event()
+  object OtpInputSuccessful : Event()
+
+  object PinIntroContinueRequest : Event()
+  object PinCreateSuccess : Event()
+}
+
+// Идеи:
+// * У Spring невозможно сказать: source(S1).target(S2).eventIs(Event.Something::class), чтобы потом
+//   в guard-е уже разобраться, хотим этот event или нет. Приходится заводить несколько event-ов
+//   Или это лучше?
+//   UPD: вместо этого они посылают с каждым event-ом payload, его можно достать из StateContext
 
 fun mainSpring(args: Array<String>) {
   runApplication<SpringdemoApplication>(*args)
--- src/main/kotlin/com/github/dimsuz/playground/routing/springdemo/StateMachineConfig.kt
+++ src/main/kotlin/com/github/dimsuz/playground/routing/springdemo/StateMachineConfig.kt
@@ -3,22 +3,24 @@ package com.github.dimsuz.playground.routing.springdemo
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.Message
+import org.springframework.statemachine.StateMachine
 import org.springframework.statemachine.config.EnableStateMachine
-import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer
 import org.springframework.statemachine.listener.StateMachineListener
 import org.springframework.statemachine.listener.StateMachineListenerAdapter
 import org.springframework.statemachine.state.State
+import org.springframework.statemachine.transition.Transition
 import java.util.*
 
 @Configuration
 @EnableStateMachine
-class StateMachineConfig : EnumStateMachineConfigurerAdapter<States, Events>() {
+class StateMachineConfig : StateMachineConfigurerAdapter<Route, Event>() {
 
   @Throws(Exception::class)
-  override fun configure(config: StateMachineConfigurationConfigurer<States, Events>) {
+  override fun configure(config: StateMachineConfigurationConfigurer<Route, Event>) {
     config
       .withConfiguration()
       .autoStartup(true)
@@ -26,31 +28,90 @@ class StateMachineConfig : EnumStateMachineConfigurerAdapter<States, Events>() {
   }
 
   @Throws(Exception::class)
-  override fun configure(states: StateMachineStateConfigurer<States, Events>) {
+  override fun configure(states: StateMachineStateConfigurer<Route, Event>) {
     states
       .withStates()
-      .initial(States.SI)
-      .states(EnumSet.allOf(States::class.java))
+      .initial(Route.Login)
+      .states(EnumSet.allOf(Route::class.java))
   }
 
   @Throws(Exception::class)
-  override fun configure(transitions: StateMachineTransitionConfigurer<States, Events>) {
+  override fun configure(transitions: StateMachineTransitionConfigurer<Route, Event>) {
     transitions
-      .withExternal()
-      .source(States.SI).target(States.S1).event(Events.E1)
+      .withLocal()
+      .source(Route.PinIntro).target(Route.PinIntro).event(Event.BackPressed)
       .and()
       .withExternal()
-      .source(States.S1).target(States.S2).event(Events.E2)
+      .source(Route.Login).target(Route.OtpIntro).event(Event.LoginSuccessOtpRequired)
+      .and()
+      .withExternal()
+      .source(Route.Login).target(Route.PinIntro).event(Event.LoginSuccessOtpNotRequired)
+      .and()
+      .withExternal()
+      .source(Route.Login).target(Route.Exit).event(Event.BackPressed)
+      .and()
+
+      .withExternal()
+      .source(Route.OtpIntro).target(Route.OtpInput).event(Event.OtpIntroContinueRequest)
+      .and()
+      .withExternal()
+      .source(Route.OtpIntro).target(Route.Login).event(Event.BackPressed)
+      .and()
+
+      .withExternal()
+      .source(Route.OtpInput).target(Route.PinIntro).event(Event.OtpInputSuccessful)
+      .and()
+      .withExternal()
+      .source(Route.OtpInput).target(Route.OtpIntro).event(Event.BackPressed)
+      .and()
+
+      .withExternal()
+      .source(Route.PinIntro).target(Route.PinCreate).event(Event.PinIntroContinueRequest)
+      .and()
+      .withExternal()
+      .source(Route.PinIntro).target(Route.Login).event(Event.BackPressed)
+      .and()
+
+      .withExternal()
+      .source(Route.PinCreate).target(Route.Main).event(Event.PinCreateSuccess)
+      .and()
+      .withExternal()
+      .source(Route.PinCreate).target(Route.PinIntro).event(Event.BackPressed)
+      .and()
+
+      .withExternal()
+      .source(Route.Main).target(Route.Exit).event(Event.BackPressed)
   }
 
   @Bean
-  fun listener(): StateMachineListener<States, Events> {
-    return object : StateMachineListenerAdapter<States, Events>() {
-      override fun eventNotAccepted(event: Message<Events>) {
-        println("Event not accepted ${event.payload}")
+  fun listener(): StateMachineListener<Route, Event> {
+    return object : StateMachineListenerAdapter<Route, Event>() {
+
+      override fun stateMachineError(
+        stateMachine: StateMachine<Route, Event>?,
+        exception: java.lang.Exception?
+      ) {
+        println("state machine error")
+        exception?.printStackTrace()
       }
-      override fun stateChanged(from: State<States, Events>?, to: State<States, Events>?) {
-        println("State change to " + to!!.id)
+
+      override fun eventNotAccepted(event: Message<Event>) {
+        System.err.println("Event not accepted ${event.payload}")
+      }
+
+      override fun transitionStarted(transition: Transition<Route, Event>?) {
+        println("Transition started. Trigger: ${transition?.trigger?.event?.javaClass?.simpleName}")
+      }
+
+      override fun stateChanged(from: State<Route, Event>?, to: State<Route, Event>?) {
+        println(
+          """
+          State change:
+            from ${from?.id}
+            to ${to?.id}
+          
+          """.trimIndent()
+        )
       }
     }
   }
